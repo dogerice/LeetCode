7a7e3d23a0960eb701a72915f18f4119cde301d1
liaohongbing <liaohongbing@hisense.com>
2019-04-16 16:10:45 +0800

    first commit/init

---
+++ src/algorithm/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package algorithm.java;
+
+/**
+ * @Auther:liaohongbing@hisense.com
+ * @date:2019/4/16
+ * @des
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
